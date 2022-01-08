2941c7e5684d733eaacecd8fda3c0bf85d43e311
Jonathan Parker <jprkr21@gmail.com>
2022-01-07 19:59:07 -0600

    creating engineer test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,25 @@
+const Engineer = require('../lib/Engineer')
+
+describe('Engineer', () => {
+
+    it('should return the github of an engineer', () => {
+
+        const github = 'jprkr21'
+        const githubName = new Engineer(github)
+        expect(githubName.getGithub()).toBe(github)
+
+    })
+
+})
+
+describe('Engineer', () => {
+
+    it('should call the role function', () => {
+
+        const roleCall = 'Software Engineer'
+        const jobPosition = new Engineer('Jonathan', 1234567, 'jonathan@email.com', roleCall)
+        expect(jobPosition.getRole()).toBe(roleCall)
+
+    })
+
+})
