691a9aa23a974fbc8d5e8301ffda36e4bcf58b93
Jonathan Parker <jprkr21@gmail.com>
2022-01-16 17:17:08 -0600

    editing index file

--- index.js
+++ index.js
@@ -1,88 +1,109 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const Manager = require('./lib/Manager')
-const Intern = require('./lib/Intern')
-const Engineer = require('./lib/Engineer')
-const { pageTemplate } = require('./src/template')
-var answers = []
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const { pageTemplate } = require('./src/template');
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+var answers = [];
 
-const questions = [
-    {
-        type: 'input',
-        name: 'employeeName',
-        message: 'Please enter an employee name.'
-    },
-    {
-        type: 'input',
-        name: 'employeeId',
-        message: 'Please enter the employee ID.'
-    },
-    {
-        type: 'input',
-        name: 'employeeEmail',
-        message: 'Please enter the employee email address.'
-    },
-    {
-        type: 'list',
-        name: 'employeeRole',
-        message: 'Please enter the employee position.',
-        choices: [ 'Manager', 'Engineer', 'Intern' ]
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Please enter the employee office number.'
-    },
-    {
-        type: 'input',
-        name: 'gitHubUsername',
-        message: 'Please enter the engineer GitHub username.'
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'Please enter the intern school.'
-    },
-];
+function questions() {
 
-function writeToFile(answers) {
-    
-    fs.writeFile('./Team.html', pageTemplate(answers)), (err) =>
-            err ? console.error(err) : console.log('Generating HTML!');
-
-}
-
-function init() {
-
-    inquirer.prompt(questions)
-    .then((res) => {
-        if (res.role === 'Manager') {
-            answers.push(new Manager(res.name, res.id, res.email, res.officeNumber))
-        } else if (res.role === 'Engineer') {
-            answers.push(new Engineer(res.name, res.id, res.email, res.gitHubUsername))
-        } else if (res.role === 'Intern') {
-            answers.push(new Intern(res.name, res.id, res.email, res.school))
-        }
-        inquirer.prompt([
+    inquirer
+        .prompt([
             {
                 type: 'list',
-                name: 'finalQuestion',
-                message: 'Would you like to add more employees?',
-                choices: [ 'Yes', 'No']
+                name: 'employeeRole',
+                message: 'Please enter the employee position.',
+                choices: [ 'Manager', 'Engineer', 'Intern' ]
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Please enter an employee name.'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please enter the employee ID.'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter the employee email address.'
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter the employee office number.',
+                when: (response) => response.employeeRole === 'Manager',
+            },
+            {
+                type: 'input',
+                name: 'gitHubUsername',
+                message: 'Please enter the engineer GitHub username.',
+                when: (response) => response.employeeRole === 'Engineer',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Please enter the intern school.',
+                when: (response) => response.employeeRole === 'Intern',
             }
         ])
-        .then((final) => {
-            if (final.finalQuestion === 'Yes') {
-                init() // calling init function to continue adding more employees
-            } else if (final.finalQuestion === 'No') {
-                writeToFile(answers) // when finished with questions, will write to the HTML file
+        .then ((response) => {
+            if (response.employeeRole === 'Manager') {
+                answers.push(
+                    new Manager(
+                        response.name,
+                        response.id, 
+                        reponse.email,
+                        reponse.officeNumber
+                    )
+                )
+            } else if (response.employeeRole === 'Engineer') {
+                answers.push(
+                    new Engineer(
+                        response.name,
+                        response.id, 
+                        reponse.email,
+                        reponse.gitHubUsername
+                    )
+                )
+            } else if (response.employeeRole === 'Intern') {
+                answers.push(
+                    new Intern(
+                        response.name,
+                        response.id, 
+                        reponse.email,
+                        reponse.school
+                    )
+                )
             }
+            inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    name: 'addEmployees',
+                    message: 'Would you like to add more employees?',
+                    choices: [ 'Yes', 'No' ]
+                }
+            ])
+            .then((finalAnswer) => {
+                if (finalAnswer.addEmployees === 'Yes') {
+                    questions();
+                } else {
+                    fs.writeFile('team-profile.html', pageTemplate(answers), (err) => {
+                        if (err) {
+                            console.log('There was an error writing the file.' + err)
+                        } else {
+                            console.log('Page successfully written.')
+                        }
+                    })
+                }
+            })
         })
-    })
+} [
+];
 
-}
-
-init();
+questions();
